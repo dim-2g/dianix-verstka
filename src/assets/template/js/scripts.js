22cd0c39296401337c9c64d89a4786b873a17a36
dim-2g <dim-2g@yandex.ru>
2017-12-29 14:35:08 +0300

    слайдер и основные разделы

---
+++ src/assets/template/js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+    var slider__main = $(".bxslider_glav");
+    slider__main.slick({
+        dots: false,
+        fade: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        speed: '1000',
+        autoplay: true
+    });
+});
+
